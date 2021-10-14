241862f358876a17d1129ab0ada99263d31fd564
Remington Cunov <61329286+rc6166@users.noreply.github.com>
2021-10-13 23:53:33 -0500

    Changed user

--- reset.sh
+++ reset.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mysql -u root -p'GradeMe!!!' pos < ~/etl.sql
-mysql -u root -p'GradeMe!!!' pos < ~/views.sql
-mysql -u root -p'GradeMe!!!' pos < ~/index.sql
-#mysql -u root -p'GradeMe!!!' pos < ~/transactions.sql
-mysql -u root -p'GradeMe!!!' pos < ~/proc.sql
-mysql -u root -p'GradeMe!!!' pos < ~/triggers.sql
-
+mysql -u dgomillion -p'GradeMe!!!' pos < ~/etl.sql
+mysql -u dgomillion -p'GradeMe!!!' pos < ~/views.sql
+mysql -u dgomillion -p'GradeMe!!!' pos < ~/index.sql
+#mysql -u dgomillion -p'GradeMe!!!' pos < ~/transactions.sql
+mysql -u dgomillion -p'GradeMe!!!' pos < ~/proc.sql
+mysql -u dgomillion -p'GradeMe!!!' pos < ~/triggers.sql
